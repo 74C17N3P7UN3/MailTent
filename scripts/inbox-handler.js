21e64be921ad9aef747a53b1ad5cdbb9d0759880
74C17N3P7UN3 <leocalzavara05@gmail.com>
2022-11-17 21:06:18 +0100

    Updated inbox-handler
    Starring is bugged and WIP

--- scripts/inbox-handler.js
+++ scripts/inbox-handler.js
@@ -1,33 +1,122 @@
+let emailCount = document.getElementById('header-total-email')
+let inboxDiv = document.getElementById('body-emails')
+
 let inboxBtn = document.getElementById('inbox')
 let starredBtn = document.getElementById('starred')
 let draftsBtn = document.getElementById('drafts')
 let sentBtn = document.getElementById('sent')
 
+setPage(inboxBtn, 'inbox')
+setPage(starredBtn, 'starred')
+setPage(draftsBtn, 'drafts')
+setPage(sentBtn, 'sent')
+
 let refreshBtn = document.getElementById('header-refresh')
-
-setRedirect(inboxBtn, 'inbox.php')
-setRedirect(starredBtn, 'starred.php')
-setRedirect(draftsBtn, 'drafts.php')
-setRedirect(sentBtn, 'sent.php')
-
-updateInbox('inbox')
-
 refreshBtn.addEventListener('click', () => {
    location.reload()
 })
 
-function updateInbox(page) {
+updateView('inbox')
 
-   console.log(userEmails)
+function updateView(page) {
+
+   if (page != 'compose') {
+      let emails = userEmails.emails
+      let emailArr = []
+
+      emails.forEach(email => {
+         if (email.location == page)
+            emailArr.push(email)
+         if (page == 'starred' && email.starred)
+            emailArr.push(email)
+      })
+
+      emailCount.innerHTML = `Total emails: ${emailArr.length}`
+      inboxDiv.innerHTML = arrToList(emailArr)
+   }
+   else {
+      //
+   }
+
 
 }
 
-function setRedirect(btn, page) {
+function setPage(btn, page) {
 
-   if (!btn.classList.contains('selected'))
-      btn.addEventListener('click', () => {
-         saveState()
-         location.replace('/php/data/' + page)
+   btn.addEventListener('click', () => {
+      let options = document.querySelectorAll('.sidebar-option')
+      options.forEach(option => { option.classList.remove('selected') })
+      btn.classList.add('selected')
+
+      updateView(page)
+   })
+
+}
+
+/* --------------- Utils Functions--------------- */
+function arrToList(arr) {
+
+   let htmlList = ''
+
+   arr.forEach(email => {
+      // Parent div
+      htmlList += `<div id="${email.timestamp}" class="email `
+      if (!email.read) htmlList += 'unread '
+      htmlList += 'no-select">'
+      // Star icon
+      htmlList += '\n\t<div class="email-star">'
+      if (!email.starred)
+         htmlList += '<i class="fa-regular fa-star"></i>'
+      else htmlList += '<i class="fa-solid fa-star"></i>'
+      htmlList += '</div>'
+      // Recipients
+      htmlList += '\n\t<div class="email-recipients">You, '
+      email.recipients.forEach(recipient => {
+         htmlList += recipient + ' '
       })
+      htmlList += '</div>'
+      // Content
+      htmlList += '\n\t<div class="email-content">'
+      // Object
+      htmlList += '\n\t\t<span class="email-object bold">' +
+         email.object +
+         '</span>'
+      // Text
+      htmlList += '\n\t\t<span class="email-text">-</span>' +
+         `\n\t\t<span class="email-text">${email.text}</span>`
+      htmlList += '\n\t</div>'
+      // Date
+      htmlList += '\n\t<div class="email-date">' +
+         unixToDate(email.timestamp) +
+         '</div>'
+      htmlList += '\n</div>'
+   })
+
+   return htmlList
+
+}
+
+function unixToDate(unixTime) {
+
+   let months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+   ]
+
+   let date = new Date(unixTime * 1000)
+
+   if (new Date().toDateString() == date.toDateString())
+      return date.getHours() + ':' + date.getMinutes()
+   return date.getDate() + ' ' + months[date.getMonth()]
 
 }
