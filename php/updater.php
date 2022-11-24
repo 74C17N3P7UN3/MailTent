3fccc2a7d38c48663ece9ce3fbd6789594ca9281
74C17N3P7UN3 <leocalzavara05@gmail.com>
2022-11-24 19:12:46 +0100

    Fixed desync of server mails
    Have to wait for beta testers to actually try

--- php/updater.php
+++ php/updater.php
@@ -7,7 +7,30 @@ session_start();
 
 if (!empty($_POST['emails'])) {
    $fileName = '../database/users/' . $_SESSION['email'] . '.json';
-   $contents = $_POST['emails'];
+
+   // Compare new server emails
+   $pointerFile = fopen($fileName, 'r');
+   $serverFile = fread($pointerFile, filesize($fileName));
+   fclose($pointerFile);
+
+   $serverJson = json_decode($serverFile);
+   $clientJson = json_decode($_POST['emails']);
+   foreach ($serverJson -> {'emails'} as $email) {
+      $emailId = $email -> {'timestamp'};
+
+      $exists = false;
+      foreach ($clientJson -> {'emails'} as $clientEmail) {
+         if ($clientEmail -> {'timestamp'} == $emailId) {
+            $exists = true;
+         }
+      }
+
+      if (!$exists) {
+         array_push($clientJson -> {'emails'}, $email);
+      }
+   }
+
+   $contents = json_encode($clientJson);
 
    $pointerFile = fopen($fileName, 'w');
    fwrite($pointerFile, $contents . "\n");
